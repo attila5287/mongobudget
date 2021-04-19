9587ff436afe7cd0b20401bcbb5f97ab5a6a7191
attila5287 <atiturkoz@hotmail.com>
2021-04-19 05:39:27 -0600

    try catch

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const demo = require( '../demo' );
 
 router.get( '/', async ( req, res ) => {
   let all = null;
-
+  
   const icons = {
     in: 'arrow-up text-success',
     out: 'arrow-down text-danger'
@@ -14,27 +14,22 @@ router.get( '/', async ( req, res ) => {
     .sort( {
       date: -1
     } )
-    .catch( e => console.log( e ) );
+    .catch( e => console.log(e));
 
-  if ( mods.length ) {
+  if (mods.length) {
     all = mods.map( ( d ) => {
       return {
         ...d.toJSON(),
         icon: icons[ d.category ]
       }
     } );
-
+  
   } else {
-
-    all = demo.map( ( d ) => {
-      return {
-        ...d,
-        icon: icons[ d.category ]
-      }
-    } );
-
+  
+    all = demo.map( ( d ) => { return { ...d, icon: icons[ d.category ] } } );
+    
   }
-
+  
   res.render( 'dashboard', {
     data: all,
     icons: icons
@@ -46,50 +41,45 @@ router.get( "/api/transaction/seed", async ( req, res ) => {
 
   if ( existing.length ) {
     const msg = {
-      status: "records exist, no seeding necessary"
+      status : "records exist, no seeding necessary"
     }
     res.json( msg );
 
   } else {
-
+    
     const bulk = await Transaction
       .insertMany( demo )
-      .catch( e => console.log( e ) );
-
+      .catch( e => console.log(e) );
+  
     res.redirect( '/' );
-
+    
   }
 } );
 
 
 router.post( "/api/transaction", async ( req, res ) => {
   const t = await Transaction
-    .create( req.body )
-    .catch( e => console.log( e ) );
-
+  .create( req.body )
+  .catch( e => console.log( e ) );
+  
   console.log( 't :>> ', t );
   res.json( t );
-
+  
 } );
 
 router.get( "/api/transaction", async ( req, res ) => {
-  const mods = await Transaction.find( {} )
-    .sort( {
-      date: -1
-    } )
-    .catch( e => console.log( '\n' + e ) );
-
-  if ( mods ) {
-    if ( mods.length ) {
+  try {
+      const mods = await Transaction.find( {} )
+        .sort( { date: -1 } )
+        .catch( e => console.log( '\n' + e ) )
+        ;
       res.json( mods );
-    } else {
-      res.json( demo )
-
+      
+    } catch (error) {
+      console.log(error);
+      res.json(demo)
     }
-  } else {
-    res.json( demo )
-  }
-
+    
 } );
 
 module.exports = router;
