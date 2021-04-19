feca39ae615e1bb41692afb8a303ac90abda2097
attila5287 <atiturkoz@hotmail.com>
2021-04-19 01:34:17 -0600

    seed with if else and install mongodb after mongoose

--- controllers/index.js
+++ controllers/index.js
@@ -4,30 +4,52 @@ const demo = require( '../demo' );
 
 router.get( '/', async ( req, res ) => {
 
-  // const mods = await Transaction
-  //   .find( {} )
-  //   .sort( {
-  //     date: -1
-  //   } )
-  //   .catch( err => res.status( 400 ).json( err ) );
-  
-    
+  const mods = await Transaction
+    .find( {} )
+    .sort( {
+      date: -1
+    } )
+    .catch( err => res.status( 400 ).json( err ) );
+
+
   const icons = {
     in: 'arrow-up text-success',
     out: 'arrow-down text-danger'
   };
-  // const all = mods.map( ( d ) => {return { ...d.toJSON(), icon: icons[d.category] } } );
-  const all = demo.map( ( d ) => {return { ...d, icon: icons[d.category] } } );
-  
-  res.render( 'dashboard', { data: all, icons:icons } );
+  const all = mods.map( ( d ) => {
+    return {
+      ...d.toJSON(),
+      icon: icons[ d.category ]
+    }
+  } );
+  // const all = demo.map( ( d ) => {return { ...d, icon: icons[d.category] } } );
+
+  res.render( 'dashboard', {
+    data: all,
+    icons: icons
+  } );
 } );
 
 router.get( "/api/transaction/seed", async ( req, res ) => {
-  const bulk = await Transaction
-    .insertMany( demo )
-    .catch( err => res.status( 400 ).json( err ) );
+  const db_records = await
+    Transaction.find( {} )
+    .catch( e => console.log( e ) );
+    
+    console.log( db_records.length );
 
-  res.json( bulk );
+  if ( db_records.length >0) {
+    const msg = { status: 'records exists' };
+    
+    res.json(msg);
+
+  } else {
+
+    const bulk = await Transaction
+      .insertMany( demo )
+      .catch( err => res.status( 400 ).json( err ) );
+
+    res.json( bulk );
+  }
 } );
 
 router.get( "/api/transaction", ( req, res ) => {
