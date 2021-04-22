c774e133adfb14f71d1a2a54007d89d15664356f
attila5287 <atiturkoz@hotmail.com>
2021-04-22 13:07:46 -0600

    indexedDB

--- public/js/ref/starter.js
+++ public/js/ref/starter.js
@@ -1,41 +1,26 @@
 let db;
-// create a new db request for a "BudgetDB" database.ex17
-const request = window.indexedDB.open("budget_db", 1);
+// create a new db request for a "BudgetDB" database.
 
 request.onupgradeneeded = function (event) {
-  // create object store called "BudgetStore" and set autoIncrement to true ex19
-  const db = event.target.result;
-
-  const objectStore = db.createObjectStore( "budget_store", { autoIncrement: true } );
-
+  // create object store called "BudgetStore" and set autoIncrement to true
 };
 
 request.onsuccess = function (event) {
   db = event.target.result;
-  console.log( ':>> db open req success: ' );
-  console.log( ':>> ' + db.name );
 
-  if ( window.navigator.onLine ) {
+  if (navigator.onLine) {
     checkDatabase();
   }
 };
 
 request.onerror = function (event) {
   // log error here
-  console.log(`:>> Error: ${event.target.errorCode}`);
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
-  var trans1 = db.transaction("foo", "readwrite");
   // access your pending object store
-  var objectStore1 = trans1.objectStore("foo")
-  
-  var newItem = [ { description: "budget change", amount: 1, category: "in", } ];
-  
   // add record to your store with add method.
-  objectStore1.add( new_item[ 0 ] );
-  
 }
 
 function checkDatabase() {
@@ -65,6 +50,3 @@ function checkDatabase() {
 
 // listen for app coming back online
 window.addEventListener('online', checkDatabase);
-
-
-// https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction
